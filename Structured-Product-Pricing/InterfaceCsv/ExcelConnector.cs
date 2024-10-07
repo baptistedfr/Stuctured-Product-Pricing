b125e78a0e9e9e2467b9e5fee111065742c372e3
Thiboss <thibault.charbonnier@edu.devinci.fr>
2024-10-07 17:16:36 +0200

    COM Excel + PricingJob

---
+++ Structured-Product-Pricing/InterfaceCsv/ExcelConnector.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace InterfaceExcel
+{
+    internal class ExcelConnector
+    {   
+        private Excel.Application Application;
+        private Excel.Workbook Workbook { get; set; }
+        private Excel.Worksheet InputSheet { get; set; }
+        private Excel.Worksheet OutputSheet { get; set; }
+
+        public ExcelConnector(string excelFilePath)
+        {
+            Application = new Excel.Application();
+            Workbook = Application.Workbooks.Open(excelFilePath);
+            InputSheet = (Worksheet)Workbook.Sheets["Inputs"];
+            OutputSheet = (Worksheet)Workbook.Sheets["Outputs"];
+            Application.Visible = true;
+        }
+        public void WriteOutput(List<PricingJob> pricingJobs, string pathFile)
+        {
+            int currentColumn = 1;
+
+            foreach (var job in pricingJobs)
+            {
+                int currentRow = 1;
+                Type type = job.GetType();
+                foreach (PropertyInfo prop in type.GetProperties())
+                {
+                    OutputSheet.Cells[currentRow, currentColumn].Value = prop.Name.ToString();
+                    OutputSheet.Cells[currentRow, currentColumn+1].Value = prop.GetValue(job).ToString();
+
+                    currentRow++;
+                }
+
+                currentColumn += 3;
+            }
+            //PrintExcel();
+            CloseAndSave(pathFile);
+        }
+
+        public void PrintExcel()
+        {
+            for(int i=1; i<10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    var contenu = OutputSheet.Cells[i, j].Value;
+                    if (contenu is not null) Console.WriteLine(contenu);
+                }
+            }
+        }
+
+        private void CloseAndSave(string oldPath)
+        {
+            try
+            {
+                string newFilePath = oldPath.Substring(0, oldPath.Length - 5) + "_Outputs.xlsx";
+                Workbook.SaveAs(newFilePath);
+                Workbook.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while saving : " + ex.Message);
+            }
+            finally
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
---
+++ Structured-Product-Pricing/InterfaceCsv/PricingJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExcel
+{
+    internal class PricingJob
+    {
+        public Int16 JobNumber { get; set; }
+        public string ProductType { get; set; }
+        public double Price { get; set; }
+        public double PricingTime { get; set; }
+
+        public PricingJob(Int16 number, string type, double price, double pricingtime)
+        {
+            JobNumber = number;
+            ProductType = type;
+            Price = price;
+            PricingTime = pricingtime;
+        }
+    }
+}
